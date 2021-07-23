33dab8bb49dca9081cd359a3165ee17ffb70961e
abhibhan212 <abhibhan@sas.upenn.edu>
2021-07-23 09:58:42 -0600

    Initial check-in of module XAMLParser

---
+++ XAMLParser/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Xaml;
+
+
+
+
+
+namespace XAMLParser
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("/Users/abhibhandari/Desktop/JOLT/xaml_outputs");
+            string[] fileContents = new string[fileNames.Length];
+            object[] xamlGraphs = new object[fileNames.Length];
+
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                Console.WriteLine(i);
+                fileContents[i] = File.ReadAllText(fileNames[i]);
+            }
+
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                xamlGraphs[i] = XamlServices.Parse(fileContents[i]);
+            }
+
+        }
+    }
+}
